ac904244b8e40e88f2917e59fff5be42b54e1ff1
Karl Otness <karl@karlotness.com>
2019-07-21 13:43:50 -0500

    Add package commentary describing the features of the macro.

--- flymake-quickdef.el
+++ flymake-quickdef.el
@@ -24,7 +24,19 @@
 
 ;;; Commentary:
 
-;;
+;; This package mainly defines `flymake-quickdef-backend', a macro
+;; which helps remove some of the boilerplate code from defining new
+;; Flymake backend functions. Consult the function's documentation for
+;; full information on use. The macro defines a function which is
+;; suitable for use with `flymake-diagnostic-functions' and handles
+;; running the external process, creating and removing any necessary
+;; files and buffers, and regex matches against diagnostic output.
+
+;; Users defining a new check function with the macro provide lisp
+;; forms giving the command line arguments of the external process, a
+;; regular expression to search its output, and lisp forms to
+;; processes the regex matches and produce arguments for
+;; `flymake-make-diagnostic'.
 
 ;;; Code:
 
