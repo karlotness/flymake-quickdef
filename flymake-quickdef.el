2f98af43d66789524937298dd9ac9925e2d40ce2
Karl Otness <karl@karlotness.com>
2019-07-21 22:41:50 -0500

    Fix capitalization in package commentary.

--- flymake-quickdef.el
+++ flymake-quickdef.el
@@ -32,9 +32,9 @@
 ;; running the external process, creating and removing any necessary
 ;; files and buffers, and regex matches against diagnostic output.
 
-;; Users defining a new check function with the macro provide lisp
+;; Users defining a new check function with the macro provide Lisp
 ;; forms giving the command line arguments of the external process, a
-;; regular expression to search its output, and lisp forms to
+;; regular expression to search its output, and Lisp forms to
 ;; processes the regex matches and produce arguments for
 ;; `flymake-make-diagnostic'.
 
